0a9b80c1524c0b3951bd1645d129c6d919c5d454
AYO SALU 2020 <ayodelesalu@syssoftcons.net>
2022-02-24 15:12:51 +0100

    init data set for jaws db

--- init_01.php
+++ init_01.php
@@ -4,12 +4,12 @@ ini_set('display_errors',true);
 
 //header('Content-Type: text/plain; charset=utf-8');
 
-/*include_once("./classes/connect.php");
+include_once("./classes/connect.php");
 include_once("./classes/passwordprotocol.php");
 include_once("./classes/uaccount.php");
 include("./functions/functions00.php");
 
-$_server = "localhost:3307";
+/*$_server = "localhost:3307";
 $_username = "root";
 $_password = "";
 $_database = "simba_db";
@@ -17,13 +17,13 @@ $_database = "simba_db";
 
 //require "vendor/ait"
 //$uurl = 'mysql://m8tblwbpbuzeuuq7:sqsgqjs9e3c5ngng@ilzyz0heng1bygi8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/je3ou4murhtmni1n';
-//$uurl = "mysql://m645bjovj4jgx7c0:ul3c5ywzkm7e128p@ilzyz0heng1bygi8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/xw33xn96e8tl958g";
-$uurl = 'JAWSDB_URL';
+$uurl = "mysql://m645bjovj4jgx7c0:ul3c5ywzkm7e128p@ilzyz0heng1bygi8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/xw33xn96e8tl958g";
+//$uurl = 'JAWSDB_URL';
 //file_put_contents("php://stderr", "something happened!");
-//$env_var = getenv($uurl,false);
+$env_var = getenv($uurl);
 //$env_var = $_SERVER[$uurl];
 echo "<pre> set";
-var_dump($_SERVER);
+var_dump($env_var);
 echo "</pre>";
 
 $_server = "ilzyz0heng1bygi8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306";
