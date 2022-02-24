007dafbab30262f13c4b0edeecb7e790223c7ff8
AYO SALU 2020 <ayodelesalu@syssoftcons.net>
2022-02-24 13:29:46 +0100

    init data set for jaws db

--- init_01.php
+++ init_01.php
@@ -10,11 +10,16 @@ $_password = "";
 $_database = "simba_db";
 */
 
+$env_var = getenv('mysql2://m8tblwbpbuzeuuq7:sqsgqjs9e3c5ngng@ilzyz0heng1bygi8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/je3ou4murhtmni1n');
+echo "<pre> set";
+var_dump($env_var);
+echo "</pre>";
+
 $_server = "ilzyz0heng1bygi8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306";
 $_username = "m8tblwbpbuzeuuq7";
 $_password = "sqsgqjs9e3c5ngng";
 
 $_database = "je3ou4murhtmni1n";
-
+//echo "host:$_server <br> ";
 $connect = new connect($_server, $_username, $_password, $_database);
 echo "connect:: " . $connect->connect_db();
