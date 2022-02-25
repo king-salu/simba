0a983872b42fdf658d61d10017294663a9a930e5
king-salu <58208677+king-salu@users.noreply.github.com>
2022-02-25 03:45:38 +0100

    Update connect.php
    debug 03

--- classes/connect.php
+++ classes/connect.php
@@ -54,13 +54,14 @@ class connect
             if ($insert) {
                 $query = "(";
                 $columns = array_keys($rdata);
+                 print_r($rdata); die();
                 foreach ($columns as $key => $column) {
                     //$query .= "`$column`" . ($key + 1 < count($columns) ? "," : "");
                     $query .= "'".htmlentities($value,ENT_IGNORE)."'" . ($key + 1 < count($values) ? "," : "");
                 }
                 $query .= ") VALUES (";
 
-                print_r($rdata); die();
+               
                 $values = array_values($rdata);
                 foreach ($values as $key => $value) {
                     $query .= "'$value'" . ($key + 1 < count($values) ? "," : "");
