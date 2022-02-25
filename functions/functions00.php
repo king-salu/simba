bbe8c3373c906cdbc063dc75796bc136558b0e14
king-salu <58208677+king-salu@users.noreply.github.com>
2022-02-25 02:21:51 +0100

    Update functions00.php
    debug 03

--- functions/functions00.php
+++ functions/functions00.php
@@ -1,5 +1,4 @@
 <?php
-header("content-type: text/html; charset=ISO-8859-1");
 include_once('./init_01.php');
 
 
@@ -24,7 +23,7 @@ function get_userinfo($_userid = '', $_email = '')
 
     $dqry = $dqry0 . $dqry1 . $dqry2;
 
-   echo "query: $dqry "; // die();
+  // echo "query: $dqry ";  die();
     $user_dets = $connect->exec_query($dqry);
 
     if (!empty($user_dets)) {
@@ -57,8 +56,8 @@ function save_userinfo($_userid, $rdata = array())
     }
 
     if ($dqry != "") {
-        echo $dqry;
-        //$connect->exec_query($dqry);
+        //echo $dqry;
+        $connect->exec_query($dqry);
         $_status = true;
     }
 
