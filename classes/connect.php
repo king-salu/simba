b3b4448a804b061aff775b908c0adb671ba9106c
king-salu <58208677+king-salu@users.noreply.github.com>
2022-02-25 16:15:09 +0100

    Update connect.php
    debug 04

--- classes/connect.php
+++ classes/connect.php
@@ -36,11 +36,16 @@ class connect
         $status = false;
         $db_inuse = $this->database;
         if (trim($db) != "") $db_inuse = $db;
+        try{
         $this->conn = new mysqli($this->servername,$this->username,$this->password,$db_inuse);
         $status = true;
         if (mysqli_connect_errno()) {
             echo "Connection failed: " .mysqli_connect_error();
             $status = false;
+        }
+        }
+        catch(Exception $ex){
+            echo "Connection failed: " . $ex->getMessage();
         }
 
         return $status;
