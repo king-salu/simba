e590774a706cfeab2d8cf3d5a64abfdd7736d72a
king-salu <johndele94@gmail.com>
2022-02-25 22:25:05 +0100

    m

--- functions/functions00.php
+++ functions/functions00.php
@@ -71,7 +71,7 @@ function save_userinfo($_userid, $rdata = array())
 
     if ($dqry != "") {
         echo $dqry; //die();
-        $connect->exec_nquery($dqry);
+        $connect->exec_query($dqry);
         $_status = true;
     }
 
